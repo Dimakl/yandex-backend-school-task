05913970bd6f30c0a84306360632b9cf56628050
Dmitrii Kocheshkov <dimakid123@gmail.com>
2021-03-29 16:07:06 +0300

    Now DEBUG in settings is gotten from the .env file

--- yandex_service/settings.py
+++ yandex_service/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Set DEBUG to 1 in .env in case you want to run in debug mode
+DEBUG = os.getenv('DEBUG') == 1
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
