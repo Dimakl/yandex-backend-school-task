ac1aa3644a41250e8364f04dd721d4e392a12f6f
Dmitrii Kocheshkov <dimakid123@gmail.com>
2021-03-29 16:07:43 +0300

    Default value to DEBUG .env

--- yandex_service/settings.py
+++ yandex_service/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Set DEBUG to 1 in .env in case you want to run in debug mode
-DEBUG = os.getenv('DEBUG') == 1
+DEBUG = os.getenv('DEBUG', 0) == 1
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
